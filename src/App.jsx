04529a2d81c98168ac57a85c997ffc09b16f4ea8
LaryssaGabi <lary.gabriela85@gmail.com>
2024-10-15 00:29:55 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/footer/Footer"
-import Header from "./components/header/header"
+import Header from "./components/header/Header"
 import Info from "./components/info/Info"
 import InfoSection from "./components/infoSection/infoSection"
 
